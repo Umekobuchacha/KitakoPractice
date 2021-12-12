2d643f2ae845a8eeeca6cee8ae85f36290e3f1c7
Umekobuchacha <nippon.cha.cha.cha.umekobucha@gmail.com>
2021-12-12 17:54:52 +0900

    1-1～1-3

--- KitakoPractice/Program.cs
+++ KitakoPractice/Program.cs
@@ -10,6 +10,27 @@ namespace KitakoPractice
     {
         static void Main(string[] args)
         {
+            Exec1_3();
+            Console.ReadKey();
+        }
+
+        private static void Exec1_3()
+        {
+            var x = 13;
+            var y = 17;
+            Console.WriteLine("x={0},y={1}", x, y);
+
+        }
+
+        private static void Exec1_2()
+        {
+            var x = 11;
+            Console.WriteLine("x={0}", x);
+        }
+
+        private static void Exec1_1()
+        {
+            Console.WriteLine("Hello World");
         }
     }
 }
