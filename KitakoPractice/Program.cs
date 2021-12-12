3e9f793cc03fbb648570023d0b7d6ba26ee40c6c
Umekobuchacha <nippon.cha.cha.cha.umekobucha@gmail.com>
2021-12-12 18:02:40 +0900

    1-4～1-10

--- KitakoPractice/Program.cs
+++ KitakoPractice/Program.cs
@@ -10,8 +10,68 @@ namespace KitakoPractice
     {
         static void Main(string[] args)
         {
-            Exec1_3();
+            Exec1_10();
             Console.ReadKey();
+        }
+
+        private static void Exec1_10()
+        {
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("x^2={0}", x * x);
+            Console.WriteLine("x^3={0}", x * x * x);
+            Console.WriteLine("x^4={0}", x * x * x * x);
+        }
+
+        private static void Exec1_9()
+        {
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("2x={0}", 2 * x);
+            Console.WriteLine("3x={0}", 3 * x);
+            Console.WriteLine("4x={0}", 4 * x);
+
+        }
+
+        private static void Exec1_8()
+        {
+            int x = 19;
+            int y = 23;
+            int z = x * y;
+            Console.WriteLine("z={0}", z);
+
+        }
+
+        private static void Exec1_7()
+        {
+            int x = 3;
+            int y = 7;
+            int tmp = y;
+            y = x;
+            x = tmp;
+            Console.WriteLine("x={0},y={1}", x, y);
+
+        }
+
+        private static void Exec1_6()
+        {
+            int x = 7;
+            x *= 3;
+            Console.WriteLine("x={0}", x);
+            x /= 2;
+            Console.WriteLine("x={0}", x);
+
+        }
+
+        private static void Exec1_5()
+        {
+            Console.WriteLine(13 * 17);
+        }
+
+        private static void Exec1_4()
+        {
+            int x;
+            x = 13 + 17;
+            Console.WriteLine("x={0}", x);
+
         }
 
         private static void Exec1_3()
